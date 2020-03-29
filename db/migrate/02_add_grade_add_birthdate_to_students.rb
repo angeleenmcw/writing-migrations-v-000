bf8811a18984cef25de666f3276cd0b9342bb258
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-29 06:11:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_add_birthdate_to_students.rb
+++ db/migrate/02_add_grade_add_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
-    add_column :students, :birthdate, :string 
-    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string do |t|
+    add_column :students, :grade, :integer do |t|
   end
 end
