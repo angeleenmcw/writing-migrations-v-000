59578a04843e58164709394142599678f1b94a04
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-29 05:56:16 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -2,4 +2,7 @@ class ChangeDatatypeForBirthdate <ActiveRecord::Migrate[5.2]
   def up
     change_column :students, :birthdate, :datetime
   end
+  def down
+    change_column :students, :birthdate, :string
+  end
 end
