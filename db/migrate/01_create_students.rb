a4354e1ee1ca76737433732f4543abf191c7de39
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-29 04:47:50 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     sql <<-SQL
-    CREATE TABLE IF NOT EXISTS 
+    CREATE TABLE IF NOT EXISTS student 
+    id INTEGER PRIMARY KEY,
+    
 end
