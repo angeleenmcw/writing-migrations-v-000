36c600677a3e89021076c44b09cbd97c6037b69f
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-29 06:07:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 1) do
-  create_table "students"
+  create_table "students" force :cascade 
 
 end
